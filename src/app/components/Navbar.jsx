ba3488b6a0be300f206469edf65e495060ab1819
Samad Aamir <samadaamir3@gmail.com>
2026-08-29 10:42:54 +0500

    navbar completed

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,16 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
+import { Mail } from 'lucide-react'
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between p-2.5 '>
+        <div className='flex justify-between p-5.5 shadow-2xs items-center'>
             <div>Land Mark </div>
             <div className='gap-6.5 flex'>
-                <Link href="/">Home</Link >
-                <Link href="/about">About</Link >
-                <Link href="/properties">Properties</Link >
-                <Link href="/contact">Contact</Link >
+                <Link href="/" className='hover:text-orange-500 transition-all'>Home</Link >
+                <Link href="/about" className='hover:text-orange-500 transition-all'>About</Link >
+                <Link href="/properties" className='hover:text-orange-500 transition-all'>Properties</Link >
+                <Link href="/contact" className='hover:text-orange-500 transition-all'>Contact</Link >
             </div>
+            <div className='p-1.5 rounded hover:bg-black  hover:text-orange-500 transition-all'><Mail /></div>
         
 
         </div>
